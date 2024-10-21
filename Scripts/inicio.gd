ae294ca0f6f369edd1cee33dafd54c13428ca605
Keboss11 <mateosj2005@gmail.com>
2024-10-21 08:55:42 +0200

    Camara

--- Scripts/inicio.gd
+++ Scripts/inicio.gd
@@ -1,14 +1,17 @@
 extends Node2D
 
 @onready var pj_prueba: Vikingo = $PjPrueba
+@onready var camara = $Camera2D
+var player 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var player = Globales.get_player()  # Obtén el jugador desde la variable global
+	player = Globales.get_player()  # Obtén el jugador desde la variable global
 	add_child(player)  # Añade el jugador a la escena actual
 	player.position = Vector2(525, 450) # Ajusta la posición inicial del jugador
+	camara.position = player.position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	camara.position = player.position
