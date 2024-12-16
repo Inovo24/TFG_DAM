359d5cfe5bd4c5496ae4ab8dbf7de090a26ce8b3
Inovo24 <isaacnovofpb1@gmail.com>
2024-12-16 17:43:30 +0100

    Revert "Incorporación de la barra de vida"
    This reverts commit 55a407df7e55f0992c5748892bede54ce1dadcc0.

--- Scripts/Personajes.gd
+++ Scripts/Personajes.gd
@@ -118,7 +118,6 @@ var vida = 100
 var daño = 10
 var velocidad = 200
 
-
 # Control de animación con AnimationTree y AnimationPlayer
 @onready var animPlayer = $AnimationPlayer
 @onready var animation_tree = $AnimationTree
@@ -134,7 +133,6 @@ func _ready():
 	# Inicializar AnimationTree
 	animation_tree.active = true
 	switch_state(State.IDLE)
-	add_to_group("jugador")
 	
 	# Configuración de temporizadores
 	input_buffer = Timer.new()
@@ -147,8 +145,8 @@ func _ready():
 	coyote_timer.one_shot = true
 	add_child(coyote_timer)
 	coyote_timer.timeout.connect(coyote_timeout)
-	
 
+	
 
 func _physics_process(delta):
 	var input_horizontal = Input.get_axis("mover_izq", "mover_der")
