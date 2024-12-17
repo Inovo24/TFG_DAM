dd89a60dbc2cc9e2c5e65d047b81bbd9939f5f05
Inovo24 <isaacnovofpb1@gmail.com>
2024-12-17 18:47:52 +0100

    Mejora de los ajustes de sonido

--- Scripts/ajustes.gd
+++ Scripts/ajustes.gd
@@ -1,17 +1,30 @@
 extends Node2D
 
+func _ready() -> void:
+	var volGen = Globales.getVolumenGeneral()
+	var volMus = Globales.getVolumenMusica()
+	var volEne = Globales.getVolumenEnemigos()
+	
+	$NivelGeneral.text = str(volGen*100)
+	$NivelMusica.text = str(volMus*100)
+	$NivelEnemigos.text = str(volEne*100)
+	
+	$HSliderGeneral.value = volGen
+	$HSliderMusic.value = volMus
+	$HSliderEnemigos.value = volEne
+
 func _on_boton_volver_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/inicio.tscn")
 
 #Sonido
-func _on_h_slider_music_value_changed(value: float) -> void:
-	change_volume(linear_to_db(value), "Music")
-
 func _on_h_slider_general_value_changed(value: float) -> void:
-	change_volume(linear_to_db(value), "Master")
+	$NivelGeneral.text = str(value*100)
+	Globales.setVolumenGeneral(value)
+
+func _on_h_slider_music_value_changed(value: float) -> void:
+	$NivelMusica.text = str(value*100)
+	Globales.setVolumenMusica(value)
 
 func _on_h_slider_enemigos_value_changed(value: float) -> void:
-	change_volume(linear_to_db(value), "SFX")
-
-func change_volume(value, bus: String) -> void:
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index(bus), value)
+	$NivelEnemigos.text = str(value*100)
+	Globales.setVolumenEnemigos(value)
--- Scripts/globales.gd
+++ Scripts/globales.gd
@@ -2,6 +2,7 @@ extends Node
 
 var personajes = [preload("res://Personajes/vikingo.tscn"),preload("res://Personajes/valkiria.tscn"),preload("res://Personajes/arquero.tscn")]
 var personaje_actual = 0 #El 0 es el vikingo, el 1 la valkiria, y el 2 el arquero
+var volumenes = [1,1,1]
 
 var player_instance = null  # Guardará la instancia del jugador
 
@@ -10,3 +11,25 @@ func get_player():
 	if player_instance == null: # Si el jugador no ha sido instanciado aún
 		player_instance = personajes[personaje_actual].instantiate()  # Instancia el jugador
 	return player_instance
+
+#Musica voumenes
+func setVolumenGeneral(value: float):
+	volumenes[0] = value
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), linear_to_db(value))
+
+func setVolumenMusica(value: float):
+	volumenes[1] = value
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), linear_to_db(value))
+
+func setVolumenEnemigos(value: float):
+	volumenes[2] = value
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), linear_to_db(value))
+
+func getVolumenGeneral() -> float:
+	return volumenes[0]
+
+func getVolumenMusica() -> float:
+	return volumenes[1]
+
+func getVolumenEnemigos() -> float:
+	return volumenes[2]
