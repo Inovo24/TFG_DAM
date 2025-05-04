3c89f45e32b23a2f861b642cb45ece982deae870
Inovo24 <isaacnovofpb1@gmail.com>
2025-05-04 13:33:45 +0200

    Gemas arreglo

--- Scripts/Elementos/gema.gd
+++ Scripts/Elementos/gema.gd
@@ -3,6 +3,6 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if body is Characters:
 		var level = get_tree().current_scene
-		if level.has_method("collect_coin"):
-			level.collect_coin()
+		if level.has_method("collect_gem"):
+			level.collect_gem()
 		queue_free()
