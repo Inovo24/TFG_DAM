0476afba7c67a056b1a615c56385a997bf2a725b
camilo1102 <camiloandretti1102@gmail.com>
2025-05-06 17:48:42 +0200

    BloqueMovible

--- Scripts/Personajes/Personajes.gd
+++ Scripts/Personajes/Personajes.gd
@@ -19,6 +19,7 @@ var life_count = 3
 var remaining_lives
 var damage = 10
 var speed = 200
+var push_force = 80.0
 var initial_speed
 
 var thorns_touching = 0
@@ -138,6 +139,11 @@ func _physics_process(delta):
 				switch_state(State.IDLE)
 
 		move_and_slide()
+		for i in get_slide_collision_count():
+					var c = get_slide_collision(i)
+					if c.get_collider() is RigidBody2D:
+						c.get_collider().apply_central_impulse(-c.get_normal() * push_force)
+
 	else:
 		velocity.x = 0
 		switch_state(State.IDLE)
