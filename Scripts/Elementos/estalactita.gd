4518d9901bdc9850a45ee7fee2f91909326c2df2
Inovo24 <isaacnovofpb1@gmail.com>
2025-05-07 19:51:37 +0200

    Estalactita se rompe +  Mas tierra para las caidas

--- Scripts/Elementos/estalactita.gd
+++ Scripts/Elementos/estalactita.gd
@@ -1,15 +1,24 @@
 extends Node2D
 
+@onready var raycastBreak = $DetectarSueloRomperse
 @export var speed = 160.0
 var current_speed = 0.0
 @export var damage: int = 40
 
 func _physics_process(delta: float) -> void:
-	position.y += current_speed * delta
+	if current_speed != 0.0:
+		position.y += current_speed * delta
+		if raycastBreak.is_colliding():
+			break_on_impact()
 
 func fall() -> void:
 	current_speed = speed
 	await get_tree().create_timer(5).timeout
+	queue_free()
+
+func break_on_impact() -> void:
+	current_speed = 0.0
+	# Si se quiere animacion al romperse, la pones aqui @Mateo o @Camilo
 	queue_free()
 
 func _on_detect_player_body_entered(body: Node2D) -> void:
