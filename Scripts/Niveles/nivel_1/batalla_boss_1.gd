7862d6b1d1785e8313efbabd05d5a0a3d772cac4
Keboss11 <mateosj2005@gmail.com>
2025-05-09 17:13:42 +0200

    batalla boss igual que estaba antes

--- Scripts/Niveles/nivel_1/batalla_boss_1.gd
+++ Scripts/Niveles/nivel_1/batalla_boss_1.gd
@@ -1,12 +1,13 @@
-extends Node2D
+extends Levels
 @onready var camara_level1 = $Camera2D
 @onready var posicion_level1 = $Camera2D/Marker2D
 
 #Nivel1 boss
 
-func _ready():
-	'''
+func _ready():	
 	camera = camara_level1
 	initialPosition = posicion_level1
 	super._ready()
-	'''
+	
+
+	
