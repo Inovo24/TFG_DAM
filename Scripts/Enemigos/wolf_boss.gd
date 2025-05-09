ce54cb91bde7567aa45deebcc9a0053424c65e6b
Keboss11 <mateosj2005@gmail.com>
2025-05-09 12:11:35 +0200

    falta la logica de la fase 2 en el boss lvl1

--- Scripts/Enemigos/wolf_boss.gd
+++ Scripts/Enemigos/wolf_boss.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const GRAVITY = 2500
+const GRAVITY = 30000
 var speed = 100
 var chargeSpeed = 200
 
@@ -12,9 +12,9 @@ enum  Phase{ONE,TWO,THREE}
 enum State {JUMP_PREPARATION, JUMP, CHARGE_PREPARATION, CHARGING, STUNNED}
 @onready var currentPhase = Phase.ONE
 @onready var currentState = State.JUMP_PREPARATION
-
+var player
 @onready var animation_player = $AnimationPlayer
-@onready var player = get_tree().get_first_node_in_group("player")
+#@onready var player = get_tree().get_first_node_in_group("player")
 @onready var stun_cooldown = $stunCooldown
 @onready var charge_cooldown = $chargeCooldown
 @onready var jump_cooldown = $jumpCooldown
@@ -35,7 +35,7 @@ func _physics_process(delta):
 				pass
 			Phase.THREE:
 				pass
-	#print(jump_cooldown.time_left)
+	print(charge_cooldown.time_left)
 	move_and_slide()
 
 func switch_phase(newPhase):
@@ -45,7 +45,7 @@ func switch_phase(newPhase):
 			Phase.ONE:
 				phase_one()
 			Phase.TWO:
-				pass
+				phase_two()
 			Phase.THREE:
 				pass
 
@@ -57,6 +57,10 @@ func switch_state(newState):
 				jump_preparation()
 			State.JUMP:
 				jump()
+			State.CHARGE_PREPARATION:
+				charge_preparation()
+			State.CHARGING:
+				charge()
 
 func phase_one():
 	if currentState == State.JUMP_PREPARATION:
@@ -65,6 +69,8 @@ func phase_one():
 	elif currentState == State.JUMP:
 		if is_on_floor():
 			switch_state(State.JUMP_PREPARATION)
+func phase_two():
+	switch_state(State.CHARGE_PREPARATION)
 
 func jump_preparation():
 	if not jump_cooldown.is_stopped():
@@ -72,7 +78,7 @@ func jump_preparation():
 	#print("aa")
 	#animation_player.play("jump_preparation")
 	jump_cooldown.start()
-	print(jump_cooldown.time_left)
+	#print(jump_cooldown.time_left)
 func jump():
 	player = get_tree().get_first_node_in_group("player")
 	if player:
@@ -80,9 +86,34 @@ func jump():
 		#position.y = player.position.y + 100
 		position.x = player.global_position.x
 		position.y -= 200
+		receive_damage(20)
 		#print("salto")
-	
+
+func charge_preparation():
+	print("preparando carga")
+	if not charge_cooldown.is_stopped():
+		return
+	charge_cooldown.start()
+func charge():
+	print("cargando")
+func receive_damage(damage_received: int):
+	currentHealth = currentHealth - damage_received
+	print(currentHealth)
+	print(currentPhase)
+	if currentHealth <= 0:
+		print("muero")
+		queue_free()
+	elif  currentHealth <= (maxHealth * 2/3) and currentPhase != Phase.TWO:
+		print("fase 2")
+		switch_phase(Phase.TWO)
+	elif  currentHealth <= (maxHealth/ 3) and currentPhase != Phase.THREE:
+		print("fase 3")
+		switch_phase(Phase.THREE)
 
 func _on_jump_cooldown_timeout():
 	if is_on_floor():
 		switch_state(State.JUMP)
+
+
+func _on_charge_cooldown_timeout():
+	switch_state(State.CHARGING)
