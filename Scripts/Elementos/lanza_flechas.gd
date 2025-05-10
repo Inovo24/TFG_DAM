43f3269a7d7ba7e814918cbb0d8bc1a725cf0ffb
Inovo24 <isaacnovofpb1@gmail.com>
2025-05-10 16:48:45 +0200

    PinchosSubeBaja y mejora de lanza flechas

--- Scripts/Elementos/lanza_flechas.gd
+++ Scripts/Elementos/lanza_flechas.gd
@@ -3,6 +3,9 @@ extends Node2D
 @onready var arrow_scene = preload("res://Scenes/flecha.tscn")
 @onready var shoot_point = $Marker2D
 @onready var fire_timer = $TimerLanzador
+@onready var sprite_before = preload("res://Sprites/sprites_para_borradores/lanzador.png")
+@onready var sprite_after = preload("res://Sprites/sprites_para_borradores/lanzador_disparo.png")
+@onready var sprite = $Sprite2D
 
 var arrow_speed = 100
 
@@ -11,6 +14,9 @@ func _ready():
 	start_random_timer()
 
 func _on_fire_timer_timeout():
+	sprite.texture = sprite_after
+	await get_tree().create_timer(1.0).timeout
+	sprite.texture = sprite_before
 	shoot_arrow()
 	start_random_timer()
 
@@ -30,6 +36,6 @@ func shoot_arrow():
 	arrow.scale.x = abs(arrow.scale.x) * direction.x
 
 func start_random_timer():
-	var random_time = randf_range(1.0, 3.0)  # Tiempo aleatorio entre 0.5 y 2 segundos
+	var random_time = randf_range(0.0, 2.0)  # Tiempo aleatorio entre 0.5 y 2 segundos
 	fire_timer.wait_time = random_time
 	fire_timer.start()
---
+++ Scripts/Elementos/pinchos_subebaja.gd
@@ -0,0 +1,39 @@
+extends StaticBody2D
+
+@onready var spike_timer = $SpikeTimer
+var start_position: Vector2
+var up_offset: float = -10.0  #Tamaño de los pinchos, primero baja esto para estar oculto, y luego sube
+var damage = 20
+
+func _ready():
+	position.y -= up_offset
+	start_position = position
+	randomize()
+	start_random_timer()
+
+func _on_spike_timer_timeout():
+	move_spikes_up()
+
+func move_spikes_up():
+	position = start_position + Vector2(0, up_offset)
+	await get_tree().create_timer(0.5).timeout
+	move_spikes_down()
+	start_random_timer()
+
+func move_spikes_down():
+	position = start_position
+
+func _on_damage_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		body.take_damage(damage)
+		body.return_to_safe_position()
+		body.can_move = false
+		await get_tree().create_timer(0.5).timeout
+		body.can_move = true
+	elif body.is_in_group("Enemies"):
+		body.queue_free()
+
+func start_random_timer():
+	var random_time = randf_range(2.0, 5.0)  # Tiempo aleatorio entre 0.5 y 2 segundos
+	spike_timer.wait_time = random_time
+	spike_timer.start()
