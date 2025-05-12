e1d46061c9f3815609095264f24bdc31f92076a5
camilo1102 <camiloandretti1102@gmail.com>
2025-05-12 14:48:04 +0200

    Arreglado bug placa/funcionando

--- Scripts/Elementos/placa.gd
+++ Scripts/Elementos/placa.gd
@@ -1,33 +1,31 @@
 extends Area2D
 
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var anim_sprite = $AnimatedSprite2D
+var bodies_inside := []
 
-@export var activate_on_body: bool = true  # También puede ser solo jugador
-
-var bodies_inside := 0
 var is_pressed := false
 
 func _ready():
 	connect("body_entered", Callable(self, "_on_body_entered"))
 	connect("body_exited", Callable(self, "_on_body_exited"))
-	animated_sprite_2d.play("idle")
+	anim_sprite.play("idle")
 
 func _on_body_entered(body):
-	if activate_on_body and body.is_in_group("Player"):  # o cualquier otro grupo
-		bodies_inside += 1
+	if body is PhysicsBody2D and body not in bodies_inside:
+		bodies_inside.append(body)
 		_check_press()
 
 func _on_body_exited(body):
-	if activate_on_body and body.is_in_group("Player"):
-		bodies_inside -= 1
+	if body in bodies_inside:
+		bodies_inside.erase(body)
 		_check_press()
 
 func _check_press():
-	if bodies_inside > 0 and not is_pressed:
+	if bodies_inside.size() > 0 and not is_pressed:
 		is_pressed = true
-		animated_sprite_2d.play("press")
-		emit_signal("pressed")  # puedes conectar esto a puertas, trampas, etc.
-	elif bodies_inside == 0 and is_pressed:
+		anim_sprite.play("press")
+		emit_signal("pressed")
+	elif bodies_inside.size() == 0 and is_pressed:
 		is_pressed = false
-		animated_sprite_2d.play("release")
+		anim_sprite.play("release")
 		emit_signal("released")
