0ee737438c41fe87e3e12df233bb808132ea4b66
Inovo24 <isaacnovofpb1@gmail.com>
2025-05-16 11:52:36 +0200

    Creacion lvl 4 + puerta que se abre

--- Scripts/Elementos/placa.gd
+++ Scripts/Elementos/placa.gd
@@ -1,31 +1,4 @@
-extends Area2D
+extends Node2D
 
-@onready var anim_sprite = $AnimatedSprite2D
-var bodies_inside := []
-
-var is_pressed := false
-
-func _ready():
-	connect("body_entered", Callable(self, "_on_body_entered"))
-	connect("body_exited", Callable(self, "_on_body_exited"))
-	anim_sprite.play("idle")
-
-func _on_body_entered(body):
-	if body is PhysicsBody2D and body not in bodies_inside:
-		bodies_inside.append(body)
-		_check_press()
-
-func _on_body_exited(body):
-	if body in bodies_inside:
-		bodies_inside.erase(body)
-		_check_press()
-
-func _check_press():
-	if bodies_inside.size() > 0 and not is_pressed:
-		is_pressed = true
-		anim_sprite.play("press")
-		emit_signal("pressed")
-	elif bodies_inside.size() == 0 and is_pressed:
-		is_pressed = false
-		anim_sprite.play("release")
-		emit_signal("released")
+signal placa_press
+signal placa_release
---
+++ Scripts/Niveles/nivel_4/nivel_4.gd
@@ -0,0 +1,11 @@
+extends Levels
+
+@onready var level4_camera = $Camera2D
+@onready var level4_position = $Camera2D/Marker2D
+
+func _ready() -> void:
+	camera = level4_camera
+	initialPosition = level4_position
+	Globales.current_character = 2
+	super._ready()
+	
