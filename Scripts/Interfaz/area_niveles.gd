28ae48c60794c7d5d57b1ccd30083f20b7decfe9
Keboss11 <mateosj2005@gmail.com>
2025-05-22 22:10:34 +0200

    Arreglo bug camara skill vikingo

--- Scripts/Interfaz/area_niveles.gd
+++ Scripts/Interfaz/area_niveles.gd
@@ -1,7 +1,6 @@
 extends Area2D
 
 var player_in_area = false
-@onready var audio_stream_player_2d = $AudioStreamPlayer2D
 
 func _on_body_entered(_body: Node2D) -> void:
 	$TextoNiveles.visible = true
