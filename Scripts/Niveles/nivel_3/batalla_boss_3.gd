2bdb42bfa88578d0648880e67eed9b1f627bc987
camilo1102 <camiloandretti1102@gmail.com>
2025-05-23 17:53:30 +0200

    fixed

--- Scripts/Niveles/nivel_3/batalla_boss_3.gd
+++ Scripts/Niveles/nivel_3/batalla_boss_3.gd
@@ -1,15 +1,17 @@
-extends Bosses
-@onready var camara_level3 = $Camera2D
-@onready var posicion_level3 = $Marker2D
+extends Levels
 
-#Nivel1 boss
+@onready var level3_camera = $Camera2D
+@onready var level3_position = $Camera2D/Marker2D
 
-func _ready():
-	level_name = "nivel3"
-	initialPosition = posicion_level3
+func _ready() -> void:
+	camera = level3_camera
+	initialPosition = level3_position
 	super._ready()
-	
-
-	
-func return_to_level():
-	get_tree().change_scene_to_file("res://Scenes/Niveles/Nivel1/nivel_3.tscn")
+func reload_health_bar():
+	healthBar.queue_free()
+	healthBar = healthBarScene.instantiate()
+	add_child(healthBar)
+func _on_final_body_entered(body: Node2D) -> void:
+	if body is Characters:
+		save_data("nivel3")
+		get_tree().change_scene_to_file("res://Scenes/inicio.tscn")
