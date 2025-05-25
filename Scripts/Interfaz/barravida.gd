c654503fa7ae466fca8173aa606276844f94d737
Keboss11 <mateosj2005@gmail.com>
2025-05-25 15:07:31 +0200

    Actualización vidas y barra de vida

--- Scripts/Interfaz/barravida.gd
+++ Scripts/Interfaz/barravida.gd
@@ -3,7 +3,15 @@ extends CanvasLayer
 var character
 @onready var progresLiveBar = $TextureProgressBar
 @onready var live = $LabelLive
+@onready var shield_lives = $Shield_lives
+
 var live_img = preload("res://Sprites/sprites_para_borradores/cora.png")
+
+var shield_imgs = [
+	preload("res://Sprites/healt/1intento.png"),
+	preload("res://Sprites/healt/2intentos.png"),
+	preload("res://Sprites/healt/3intentos.png")
+]
 
 #Para el tiempo en los bosses
 @onready var default = 0
@@ -21,11 +29,19 @@ func _process(_delta: float) -> void:
 		$Lives/TextureRect.texture = live_img if character.life_count >= 1 else null
 		$Lives/TextureRect2.texture = live_img if character.life_count >= 2 else null
 		$Lives/TextureRect3.texture = live_img if character.life_count >= 3 else null
+		if character.life_count ==1:
+			shield_lives.texture = shield_imgs[0]
+		elif character.life_count == 2:
+			shield_lives.texture = shield_imgs[1]
+		else:
+			shield_lives.texture = shield_imgs[2]
 
 func update_bar():
 	if character:
 		progresLiveBar.value = character.getCurrentHealth()
 		live.text = str(progresLiveBar.value) + "/" + str(progresLiveBar.max_value)
+		var vidas = clamp(character.life_count, 0, 2)
+		shield_lives.texture = shield_imgs[vidas]
 
 func update_player():
 	character = Globales.get_player()
