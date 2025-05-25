ac76f158ae107ec49ddeb81dfbafe4c66eead19f
camilo1102 <camiloandretti1102@gmail.com>
2025-05-25 16:31:52 +0200

    Arreglado Bug Extends Bosses boss3 + Fondo Boss3

--- Scripts/Niveles/nivel_3/batalla_boss_3.gd
+++ Scripts/Niveles/nivel_3/batalla_boss_3.gd
@@ -1,16 +1,17 @@
-extends Levels
 
-@onready var level3_camera = $Camera2D
-@onready var level3_position = $Camera2D/Marker2D
 
-func _ready() -> void:
-	camera = level3_camera
-	initialPosition = level3_position
+extends Bosses
+
+@onready var posicion_boss3 = $Marker2D
+
+func _ready():
+	level_name = "nivel3"
+	initialPosition = posicion_boss3
 	super._ready()
-func reload_health_bar():
-	healthBar.queue_free()
-	healthBar = healthBarScene.instantiate()
-	add_child(healthBar)
+	var player = Globales.get_player()
+	player.has_checkpoint = true
+	player.checkpoint_position = posicion_boss3.position
+
 func _on_final_body_entered(body: Node2D) -> void:
 	if body is Characters:
 		get_tree().change_scene_to_file("res://Scenes/inicio.tscn")
