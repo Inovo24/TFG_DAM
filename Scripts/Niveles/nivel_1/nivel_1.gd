cdef2e3adf8152740158e4d45f76202370674942
camilo1102 <camiloandretti1102@gmail.com>
2025-06-06 08:52:41 +0200

    Musica Level 1 y Level 2

--- Scripts/Niveles/nivel_1/nivel_1.gd
+++ Scripts/Niveles/nivel_1/nivel_1.gd
@@ -2,10 +2,12 @@ extends Levels
 
 @onready var camara_level1 = $Camera2D
 @onready var posicion_level1 = $Camera2D/Marker2D
+@onready var music_player: AudioStreamPlayer2D = $MusicPlayer
 
 #Nivel1 
 
 func _ready():	
+	music_player.play()
 	camera = camara_level1
 	initialPosition = posicion_level1
 	has_boss = true
