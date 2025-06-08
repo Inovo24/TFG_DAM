0831a9647634feed26fcc5b3a0bf6b75dd54bc8f
Keboss11 <mateosj2005@gmail.com>
2025-06-08 19:24:40 +0200

    Cinematica final

---
+++ Scripts/cut_scene/cinematicalvl_5_fin.gd
@@ -0,0 +1,20 @@
+extends CinematicText
+
+
+func _ready():
+	actionNeeded = false
+	narration_data = [
+		[tr("lvl5_cine_15"), 3.0],[tr("lvl5_cine_16"), 2.5],
+		[tr("lvl5_cine_17"), 3.0],[tr("lvl5_cine_18"), 3.0],
+		[tr("lvl5_cine_19"), 3.0],[tr("lvl5_cine_20"), 3.0],
+		[tr("lvl5_cine_21"), 3.0],[tr("lvl5_cine_22"), 3.0],
+		[tr("lvl5_cine_23"), 2.5],[tr("lvl5_cine_24"), 3.0],
+		[tr("lvl5_cine_25"), 2.5],[tr("lvl5_cine_26"), 2.5],
+		[tr("lvl5_cine_27"), 3.0],[tr("lvl5_cine_28"), 3.0],
+		[tr("lvl5_cine_29"), 3.0],[tr("lvl5_cine_30"), 3.0],
+		[tr("lvl5_cine_31"), 3.0],[tr("lvl5_cine_32"), 3.0],
+		[tr("lvl5_cine_33"), 3.0],[tr("lvl5_cine_34"), 3.0],
+		[tr("lvl5_cine_35"), 3.0],[tr("lvl5_cine_36"), 3.0],
+		[tr("lvl5_cine_37"), 3.5],
+	];
+	super._ready()
